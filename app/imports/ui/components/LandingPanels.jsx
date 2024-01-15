14a9420f534104b81905b8c84fe4628c6f181b43
tliam1 <97718694+tliam1@users.noreply.github.com>
2024-01-15 10:15:07 -1000

    added a more stylistic nav bar

--- app/imports/ui/components/LandingPanels.jsx
+++ app/imports/ui/components/LandingPanels.jsx
@@ -1,52 +1,58 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Col, Button, Nav, NavItem } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
+/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LandingPanel = () => {
   const [activePanel, setActivePanel] = useState('home');
 
+  {/* SetActivePanel utilizes useState. Used to update the state variable activePanel with a new value (other panels) */}
   const handleButtonClick = (panel) => {
     setActivePanel(panel);
   };
 
   return (
     <Container id={PAGE_IDS.LANDING} className="py-3">
-      <Row>
-        <Col md={4}>
-          <Button onClick={() => handleButtonClick('home')}>Home</Button>
-        </Col>
-        <Col md={4}>
-          <Button onClick={() => handleButtonClick('getHelp')}>Get Help</Button>
-        </Col>
-        <Col md={4}>
-          <Button onClick={() => handleButtonClick('needHelp')}>Need Help</Button>
-        </Col>
-      </Row>
+      <Nav variant="underline" defaultActiveKey="/home">
+        <Nav.Item>
+          {/* Call handleButtonClick function when Home link is clicked */}
+          <Nav.Link href="" onClick={() => handleButtonClick('home')}>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          {/* Call handleButtonClick function when Give Help link is clicked */}
+          <Nav.Link onClick={() => handleButtonClick('giveHelp')}>Give Help</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          {/* Call handleButtonClick function when Need Help link is clicked */}
+          <Nav.Link onClick={() => handleButtonClick('needHelp')}>Need Help</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div>
+        <div style={{ position: 'relative', height: '200px' }} className="pt-3">
+          {/* Home Panel */}
+          {activePanel === 'home' && (
+            <div style={{ position: 'absolute', zIndex: 3 }}>
+              <h2>Home Panel</h2>
+              <p>DASHBOARD HERE</p>
+            </div>
+          )}
 
-      <div style={{ position: 'relative', height: '200px' }} className="pt-3">
-        {/* Home Panel */}
-        {activePanel === 'home' && (
-          <div style={{ position: 'absolute', zIndex: 3 }}>
-            <h2>Home Panel</h2>
-            <p>DASHBOARD HERE</p>
-          </div>
-        )}
+          {/* Get Help Panel */}
+          {activePanel === 'giveHelp' && (
+            <div style={{ position: 'absolute', zIndex: 3 }}>
+              <h2>Give Help Panel</h2>
+              <p>A SEARCH + Buttons/Cards for interesting groups</p>
+            </div>
+          )}
 
-        {/* Get Help Panel */}
-        {activePanel === 'getHelp' && (
-          <div style={{ position: 'absolute', zIndex: 3 }}>
-            <h2>Get Help Panel</h2>
-            <p>A SEARCH + Buttons/Cards for interesting groups</p>
-          </div>
-        )}
-
-        {/* Need Help Panel */}
-        {activePanel === 'needHelp' && (
-          <div style={{ position: 'absolute', zIndex: 3 }}>
-            <h2>Need Help Panel</h2>
-            <p>ALL HELP REQUEST RELATED INFO</p>
-          </div>
-        )}
+          {/* Need Help Panel */}
+          {activePanel === 'needHelp' && (
+            <div style={{ position: 'absolute', zIndex: 3 }}>
+              <h2>Need Help Panel</h2>
+              <p>ALL HELP REQUEST RELATED INFO</p>
+            </div>
+          )}
+        </div>
       </div>
     </Container>
   );
