b19abb2dd7eaaab2ac630e2da4ee42676cc816d2
Kristine Orpilla <143130393+kristineorpilla03@users.noreply.github.com>
2024-01-22 11:21:23 -1000

    Updated

--- app/imports/ui/pages/VolunteerEventOpportunities.jsx
+++ app/imports/ui/pages/VolunteerEventOpportunities.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+// import { RadioField, AutoForm } from 'uniforms-bootstrap5';
 import EventCard from '../components/EventCard';
 
 const VolunteerEventOpportunities = () => (
@@ -9,10 +10,21 @@ const VolunteerEventOpportunities = () => (
     </Row>
     <Row>
       <Col md={2} lg={2}>
-        <h3>Filter By</h3>
+        <h3>Filter By:</h3>
+        <h4>Location</h4>
+        <p>Within 1 Mile</p>
+        <p>Within 5 Miles</p>
+        <p>Within 10 Miles</p>
+        <h4>Category</h4>
+        <p>Animal Shelter</p>
+        <p>Clean Up</p>
+        <p>Food Distribution</p>
+        <h4>Need Background Check</h4>
       </Col>
       <Col>
-        <Row md={2} lg={2} className="gx-4">
+        <Row md={2} lg={2}>
+          <EventCard />
+          <EventCard />
           <EventCard />
           <EventCard />
         </Row>
