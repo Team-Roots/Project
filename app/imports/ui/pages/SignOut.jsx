c26e58f0d6199da3a15ec9a99f6496d5f299241b
Yafei Wang <143229839+UHMyafeiwang@users.noreply.github.com>
2024-01-23 12:17:23 -1000

    Update SignOut.jsx

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import Button from 'react-bootstrap/Button';
+// Removed the Button import as it's not used
 import Card from 'react-bootstrap/Card';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh', // This makes the div take the full height of the viewport
-      marginTop: '-220px',
-    }}
-    >
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="/images/Voluntree.logo.png" />
-        <Card.Body>
-          <Card.Title style={{ fontSize: '34px' }}>You signed out of your account</Card.Title>
-          <Card.Text>
-            it's a good idea to close all browser windows.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh', // This makes the div take the full height of the viewport
+        marginTop: '-220px',
+      }}
+      >
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="/images/Voluntree.logo.png" />
+          <Card.Body>
+            <Card.Title style={{ fontSize: '34px' }}>You signed out of your account</Card.Title>
+            <Card.Text>
+              it&apos;s a good idea to close all browser windows.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
   );
 };
 
