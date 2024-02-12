430a3395b6649150185a9b21f2434ccf78f06031
Destiny Shishido <itsmedest2003@gmail.com>
2024-02-12 11:07:49 -1000

    attempt to subscribe

--- app/imports/ui/pages/MyAccount.jsx
+++ app/imports/ui/pages/MyAccount.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row, Card, Image, Button } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Stuffs } from '../../api/stuff/StuffCollection';
+import PropTypes from 'prop-types';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
 
-
-/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+/* Renders the MyAccount page, displaying all user info */
 const MyAccount = () => {
-  // Get the email of current user
-  const userEmail = Meteor.user();
-
   // useTracker connects Meteor data to React components
   const { ready, account } = useTracker(() => {
+    // Get the username of current user
+    const user = Meteor.user();
+    const email = user ? user.username : null;
     // Get access to UserProfile documents.
-    const subscription = Meteor.subscribe(UserProfiles.subscribe());
+    const subscription = UserProfiles.subscribe();
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the UserProfile documents
-    const accountItems = UserProfiles.find({}, { sort: { name: 1 } }).fetch();
+    // Get the Account documents
+    let accountItems;
+    if (email) {
+      accountItems = UserProfiles._collection.find({ email: email }).fetch();
+    }
     return {
       account: accountItems,
       ready: rdy,
@@ -44,8 +47,8 @@ const MyAccount = () => {
                 roundedCircle
                 src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
               />
-              <Card.Title className="pt-3 accountcardtitle">John</Card.Title>
-              <Card.Title className="pb-3 accountcardtitle">Doe</Card.Title>
+              <Card.Title className="pt-3 accountcardtitle">{account.firstName}</Card.Title>
+              <Card.Title className="pb-3 accountcardtitle">{account.lastName}</Card.Title>
               <Card.Subtitle className="py-2 accountcardsubtitle">(808)123-4567</Card.Subtitle>
               <Card.Subtitle className="py-2 accountcardsubtitle">johndoe@gmail.com</Card.Subtitle>
               <Button className="align-bottom accountbutton">Edit Profile</Button>
