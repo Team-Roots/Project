c0af33cc99960a92d172933af418713bcdf31852
Yujie He <yujie6@hawaii.edu>
2024-02-20 18:34:52 -1000

    update event registration page

--- app/imports/ui/components/RegistrationCard.jsx
+++ app/imports/ui/components/RegistrationCard.jsx
@@ -12,7 +12,7 @@ const RegistrationCard = ({ event }) => {
     <Container>
       <Row className="justify-content-center align-items-center my-5">
         <Col md={4} className="p-0">
-          <Image className="img-fluid w-100 h-100" src="https://static.wikia.nocookie.net/aesthetics/images/0/0e/Green.jpg/revision/latest?cb=20200723215419" />
+          <Image className="img-fluid w-100 h-100" src={event.image} />
         </Col>
         <Col md={8}>
           <Card className="mt-5">
