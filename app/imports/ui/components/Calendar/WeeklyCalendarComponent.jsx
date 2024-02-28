511b15cb114d4ae152b3ac643c74206bb7b89d24
tliam1 <97718694+tliam1@users.noreply.github.com>
2024-02-28 11:46:56 -1000

    Putting items on calendar

--- app/imports/ui/components/Calendar/WeeklyCalendarComponent.jsx
+++ app/imports/ui/components/Calendar/WeeklyCalendarComponent.jsx
@@ -2,20 +2,33 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import PropTypes from 'prop-types';
 
-const WeeklyCalendar = () => (
+const WeeklyCalendar = ({ subbedEvents }) => (
   <Container>
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridWeek"
       height="auto"
       weekends
-      events={[
-        { title: 'event 1', date: '2024-02-26' },
-        { title: 'event 2', date: '2024-02-27' },
-      ]}
+      events={subbedEvents.map((sub) => ({
+        title: sub.subscriptionInfo.eventName,
+        date: sub.subscriptionInfo.eventDate,
+      }))}
+      to="/my-events"
     />
   </Container>
 );
 
+WeeklyCalendar.propTypes = {
+  subbedEvents: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      subscriptionInfo: PropTypes.shape({
+        eventName: PropTypes.string.isRequired,
+        eventDate: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 export default WeeklyCalendar;
