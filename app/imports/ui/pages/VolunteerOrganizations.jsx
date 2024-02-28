b78370f266cd10fd5297c93bba9e4ee95d3ade93
choytr <143041680+choytr@users.noreply.github.com>
2024-02-27 14:16:29 -1000

    Fixed organizations.jsx

--- app/imports/ui/pages/VolunteerOrganizations.jsx
+++ app/imports/ui/pages/VolunteerOrganizations.jsx
@@ -3,8 +3,6 @@ import { Container, Row, Col, FormCheck } from 'react-bootstrap';
 import FormCheckInput from 'react-bootstrap/FormCheckInput';
 import FormCheckLabel from 'react-bootstrap/FormCheckLabel';
 import { useTracker } from 'meteor/react-meteor-data';
-import EventCard from '../components/EventCard';
-import { Events } from '../../api/event/EventCollection';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Organizations } from '../../api/organization/OrganizationCollection';
@@ -13,20 +11,20 @@ const VolunteerOrganizations = () => {
   const { ready, organizations } = useTracker(() => {
     const subscription = Organizations.subscribeOrg();
     const rdy = subscription.ready();
-    const orgItems = Events.find().fetch();
+    const foundOrganizations = Organizations.find({}, {});
     return {
-      organizations: orgItems,
+      organizations: foundOrganizations,
       ready: rdy,
     };
   }, []);
   return (ready ? (
     <Container className="py-3" id={PAGE_IDS.LIST_ORGANIZATIONS}>
       <Row>
-        <Col lg={2}>
+        <Col>
           {organizations.map(organization => (
-            <p>
-              adasdfsadf
-            </p>
+            <h1>
+              {organization.name}
+            </h1>
           ))}
         </Col>
       </Row>
