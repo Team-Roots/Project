273a7fc1284951b42db536312e5db81ff0d465e4
Yafei Wang <143229839+UHMyafeiwang@users.noreply.github.com>
2024-02-29 13:35:39 -1000

    Update AboutUs.jsx

--- app/imports/ui/pages/AboutUs.jsx
+++ app/imports/ui/pages/AboutUs.jsx
@@ -9,22 +9,8 @@ const AboutUs = () => (
   <div>
     <h1>About Us</h1>
     <Row>
-      <Col className="text-center">
-        <Odometer value={Math.floor(Math.random() * 1000)} format="d" style={{ fontSize: '3rem' }} />
-        <h5 style={{ textDecoration: 'none' }}>Hours Served</h5>
-      </Col>
-      <Col className="text-center">
-        <Odometer value={Math.floor(Math.random() * 1000)} format="d" style={{ fontSize: '3rem' }} />
-        <h5>Persons Served</h5>
-      </Col>
-      <Col className="text-center">
-        <Odometer value={Math.floor(Math.random() * 1000)} format="d" style={{ fontSize: '3rem' }} />
-        <h5>Rides Shared</h5>
-      </Col>
-      <Col className="text-center">
-        <Odometer value={Math.floor(Math.random() * 1000)} format="d" style={{ fontSize: '3rem' }} />
-        <h5>Communities Changed</h5>
-      </Col>
+      <Odometer value={Math.floor(Math.random() * 1000)} format="d" style={{ fontSize: '3rem' }} />
+      {/* Remember to replace the Odometer value prop with dynamic or state-based values if needed */}
     </Row>
   </div>
 );
