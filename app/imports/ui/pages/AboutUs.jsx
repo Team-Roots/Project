006fc80d9a7ccdc645bed1dfacb4faf47dd5753e
josiahkila <127445228+josiahkila@users.noreply.github.com>
2024-03-03 00:08:39 -1000

    about us

--- app/imports/ui/pages/AboutUs.jsx
+++ app/imports/ui/pages/AboutUs.jsx
@@ -8,6 +8,7 @@ import './css/AboutUs.css';
 
 const VolunteeringImage = '../images/volunteerImage.jpg';
 const VolunteerStaff = '../images/VolunteerStaff.jpg';
+const VolunteerShovel = '../images/VolunteerShovel.jpg';
 
 const AboutUs = () => (
   <Container id={PAGE_IDS.ABOUT_US} className="my-5">
@@ -46,30 +47,42 @@ const AboutUs = () => (
                     <Card.Body>
                       <Card.Title>Join Our Community</Card.Title>
                       <Card.Text>
-                        Be part of a thriving community dedicated to making a difference. Whether you're looking to contribute your time, skills, or resources, there's a place for you here.
+                        Be part of a thriving community dedicated to making a difference. Whether you're looking to contribute your time, skills, or resources, there's a place for you here. With Voluntree, you can streamline your volunteer journey, expand your network,
+                        and amplify your impact, all in one place. Soon, you can join us in building a stronger,
+                        more connected community through the power of volunteering.
                       </Card.Text>
                     </Card.Body>
                   </Col>
                   <Col md={6} className="d-flex align-items-center justify-content-center">
-                    <Card.Img src={VolunteeringImage} alt="Volunteering" className="imgaa-fluid" />
+                    <Card.Img src={VolunteeringImage} alt="Volunteering" className="img-fluid" />
                   </Col>
                 </Row>
               </Card>
             </Col>
           </Row>
         </Container>
-        <br />
-        <br />
-        <FadeInSection>
-          <p>
-            With Voluntree, you can streamline your volunteer journey, expand your network,
-            and amplify your impact, all in one place. Soon, you can join us in building a stronger,
-            more connected community through the power of volunteering.
-          </p>
-        </FadeInSection>
-
-        <br />
-        <br />
+        <Container fluid className="my-5">
+          <Row>
+            <Col>
+              <Card>
+                <Row className="g-0">
+                  <Col md={6} className="d-flex align-items-center justify-content-center">
+                    <Card.Img src={VolunteerShovel} alt="Volunteering" className="img-fluid" />
+                  </Col>
+                  <Col md={6} className="d-flex align-items-center">
+                    <Card.Body>
+                      <Card.Title>WHAT ARE YOU WAITING FOR?</Card.Title>
+                      <Card.Text>
+                        Sign up today to unlock all the benefits Voluntree has to offer!
+                        <div> <Link to="/signup"> Click here to register for an account.</Link> </div>
+                      </Card.Text>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
         <FadeInSection>
           <Accordion defaultActiveKey="0" className="faq-accordion">
             <Accordion.Item eventKey="0">
@@ -84,13 +97,6 @@ const AboutUs = () => (
               <Accordion.Body>
                 Find Events that cater to your interests and abilities. Finding events are super simple with easy search methods and recommendations based on your previous volunteer work!
                 Track your volunteer progress through the user dashboard and aim to hit your monthly goals!
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>What Are You Waiting For?</Accordion.Header>
-              <Accordion.Body>
-                Sign up today to unlock all the benefits Voluntree has to offer!
-                <div> <Link to="/signup"> Click here to register for an account.</Link> </div>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
