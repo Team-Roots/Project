e1f857db5bf2a131e6b809ff747722ac2c2427d4
choytr <choytr@hawaii.edu>
2024-03-25 11:14:20 -1000

    Made mission and description fields

--- app/imports/api/organization/OrganizationCollection.js
+++ app/imports/api/organization/OrganizationCollection.js
@@ -18,6 +18,16 @@ class OrganizationCollection extends BaseCollection {
         type: String,
         defaultValue: 'John Doe Save The Turtles INC',
       },
+      missionStatement: {
+        type: String,
+        optional: true,
+        defaultValue: '',
+      },
+      description: {
+        type: String,
+        optional: true,
+        defaultValue: '',
+      },
       website: {
         type: String,
         defaultValue: 'Change Me!',
