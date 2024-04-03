fdad4dfe60534ab4f108507b8b75b727e9ea4060
josiahkila <127445228+josiahkila@users.noreply.github.com>
2024-04-03 09:28:17 -1000

    Update Publications.js

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { MATPCollections } from '../../api/matp/MATPCollections';
 import { Events } from '../../api/event/EventCollection';
+<<<<<<< HEAD
+=======
+import { Comments } from '../../api/comment/CommentCollection';
+>>>>>>> parent of 8662d24 (Merge branch 'main' into issue-136)
 // Call publish for all the collections.
 MATPCollections.collections.forEach(c => c.publish());
 
@@ -15,10 +19,32 @@ Meteor.publish(null, function () {
 });
 
 // All-level publication.
+<<<<<<< HEAD
 Meteor.publish(Events.event, function () {
   if (this.userId) {
     // return Events.collection.find({});
     // we really dont need anything here (this is causing errors too)
   }
   return this.ready();
+=======
+Meteor.publish(Events.event, function publishEvents() {
+  if (!this.userId) {
+    return this.ready();
+  }
+
+  return Events.find();
+});
+
+// eslint-disable-next-line meteor/audit-argument-checks
+Meteor.publish('userComments', function (ownerId) {
+  return Comments.find({ owner: ownerId });
+});
+
+Meteor.publish('userNames', function publishUserNames() {
+  return Meteor.users.find({}, {
+    fields: {
+      username: 1, // Publish only the usernames
+    },
+  });
+>>>>>>> parent of 8662d24 (Merge branch 'main' into issue-136)
 });
