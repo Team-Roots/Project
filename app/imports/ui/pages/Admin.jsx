fe5377e45a5a8eb32d6d2c09ddc3745524a65c72
Yafei Wang <143229839+UHMyafeiwang@users.noreply.github.com>
2024-04-14 00:13:14 -1000

    Update Admin.jsx

--- app/imports/ui/pages/Admin.jsx
+++ app/imports/ui/pages/Admin.jsx
@@ -1,48 +1,30 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import { Stuffs } from '../../api/stuff/StuffCollection';
-import StuffItemAdmin from '../components/StuffItemAdmin';
-import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { AdminProfiles } from '../../api/user/AdminProfileCollection';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-/* Renders a table containing all of the Stuff documents. Use <StuffItemAdmin> to render each row. */
 const Admin = () => {
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { stuffs, ready } = useTracker(() => {
-    // Get access to Stuff documents.
-    const subscription = Stuffs.subscribeStuffAdmin();
-    // Determine if the subscription is ready
-    const rdy = subscription.ready();
-    // Get the Stuff documents
-    const items = Stuffs.find({}).fetch();
+
+  const { ready, email } = useTracker(() => {
+    // Ensure that minimongo is populated with all collections prior to running render().
+    const sub1 = AdminProfiles.subscribe();
     return {
-      stuffs: items,
-      ready: rdy,
+      ready: sub1.ready(),
+      email: Meteor.user()?.username,
     };
   }, []);
-  return (ready ? (
-    <Container id={PAGE_IDS.LIST_STUFF_ADMIN} className="py-3">
-      <Row className="justify-content-center">
-        <Col md={7}>
-          <Col className="text-center"><h2>List Stuff (Admin)</h2></Col>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Name1</th>
-                <th>Quantity</th>
-                <th>Condition</th>
-                <th>Owner</th>
-              </tr>
-            </thead>
-            <tbody>
-              {stuffs.map((stuff) => <StuffItemAdmin key={stuff._id} stuff={stuff} />)}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
+  const profile = AdminProfiles.findOne({ email });
+  return ready ? (
+    <Container id={PAGE_IDS.ADMIN_PAGE} className="py-3">
+      <h1>Admin Profile</h1>
+      <p><strong>Email:</strong> {profile.email}</p>
+      <p><strong>First Name:</strong> {profile.firstName}</p>
+      <p><strong>Last Name:</strong> {profile.lastName}</p>
+      <p><strong>Phone Number:</strong> {profile.phoneNumber}</p>
     </Container>
-  ) : <LoadingSpinner />);
+  ) : <LoadingSpinner />;
 };
-
 export default Admin;
