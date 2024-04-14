93dfd48cab0751d65db35aa289f33737b9557e0f
Yafei Wang <143229839+UHMyafeiwang@users.noreply.github.com>
2024-04-13 23:00:20 -1000

    Update Admin.jsx

--- app/imports/ui/pages/organizations/Admin.jsx
+++ app/imports/ui/pages/organizations/Admin.jsx
@@ -60,25 +60,4 @@ const AboutUs = () => (
               </Card>
             </Col>
           </Row>
-        </Container>
-        <Container fluid className="my-5">
-          <Row>
-            <Col>
-              <Card>
-                <Row className="g-0">
-                  <Col md={6} className="d-flex align-items-center">
-                    <Card.Body>
-                      <Card.Title>Join Our Community</Card.Title>
-                      <Card.Text>
-                        {/* eslint-disable-next-line max-len,react/no-unescaped-entities */}
-                        Be part of a thriving community dedicated to making a difference. Whether you're looking to contribute your time, skills, or resources, there's a place for you here. With Voluntree, you can streamline your volunteer
-                        journey, expand your network,
-                        {/* eslint-disable-next-line react/no-unescaped-entities */}
-                        Be part of a thriving community dedicated to making a difference. Whether you're looking to contribute your time, skills, or resources, there's a place for you here. With Voluntree, you can streamline your
-                        volunteer journey, expand your network,
-                        and amplify your impact, all in one place. Soon, you can join us in building a stronger,
-                        more connected community through the power of volunteering.
-                      </Card.Text>
-                    </Card.Body>
-                  </Col>
-
+      
