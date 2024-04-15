5429eac5fb4d3b5011e5a9006c83b82dfa3c4e11
Yafei Wang <143229839+UHMyafeiwang@users.noreply.github.com>
2024-04-15 13:08:38 -1000

    Update App.jsx

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -78,7 +78,8 @@ const App = () => {
           <Route path="/add-event" element={<ProtectedRoute><AddEvent /></ProtectedRoute>} />
           <Route path="/edit-event/:_id" element={<ProtectedRoute><EditEvent /></ProtectedRoute>} />
           <Route path="/plans" element={<VoluntreeSubscriptionPlans />} />
-          <Route path="/organizations" element={<ProtectedRoute><SearchOrganizations /></ProtectedRoute>} />
+          {/* <Route path="/organizations" element={<ProtectedRoute><SearchOrganizations /></ProtectedRoute>} /> */}
+          <Route path="/organizations" element={<SearchOrganizations />} />
           <Route path="/organizations/:orgID" element={<ProtectedRoute><ViewOrganization /></ProtectedRoute>} />
           <Route path="/organizations/register" element={<ProtectedRoute><RegisterOrganization /></ProtectedRoute>} />
           <Route path="/organizations/:orgID/edit" element={<ProtectedRoute><EditOrganization /></ProtectedRoute>} />
