80968e5b9c764b271c433863f45e6b772cbcc818
choytr <143041680+choytr@users.noreply.github.com>
2024-04-19 14:42:07 -1000

    Set up basic horizontal events scroll

--- app/imports/ui/components/organizations/EditOrgGear.jsx
+++ app/imports/ui/components/organizations/EditOrgGear.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Gear } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const EditOrgGear = ({ orgID }) => (
   <Link to={`/organizations/${orgID}/edit`} style={{ color: 'black' }}><Gear size="1.25rem" /></Link>
 );
 
 EditOrgGear.propTypes = {
-  orgID: Number.isRequired,
+  orgID: PropTypes.number.isRequired,
 };
 
 export default EditOrgGear;
--- app/imports/ui/pages/organizations/ViewOrganization.jsx
+++ app/imports/ui/pages/organizations/ViewOrganization.jsx
@@ -6,8 +6,9 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../../utilities/PageIDs';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { Organizations } from '../../../api/organization/OrganizationCollection';
-import NotFound from '../NotFound';
 import { OrganizationAdmin } from '../../../api/organization/OrganizationAdmin';
+import { Events } from '../../../api/event/EventCollection';
+import NotFound from '../NotFound';
 import { COMPONENT_IDS } from '../../utilities/ComponentIDs';
 import EditOrgGear from '../../components/organizations/EditOrgGear';
 
@@ -17,18 +18,22 @@ const VolunteerOrganizations = () => {
   const navigate = useNavigate();
   const parsedOrgID = parseInt(orgID, 10);
   if (orgID) { // display just this requested organization
-    const { ready, thisOrganization, allowedToEdit } = useTracker(() => {
+    const { ready, thisOrganization, allowedToEdit, events } = useTracker(() => {
       const orgSubscription = Organizations.subscribeOrg();
       const orgAdminSubscription = OrganizationAdmin.subscribeOrgAdmin();
-      const rdy = orgSubscription.ready() && orgAdminSubscription.ready();
+      const eventSubscription = Events.subscribeEvent();
+      const rdy = orgSubscription.ready() && orgAdminSubscription.ready() && eventSubscription.ready();
       const foundOrganization = Organizations.findOne({ orgID: parsedOrgID }, {});
       const foundOrganizationAdmin = OrganizationAdmin.findOne({ orgAdmin: currentUser?.username, orgID: parsedOrgID }, {});
+      const foundEvents = Events.find({ organizationID: parsedOrgID }, {}).fetch();
       return {
         ready: rdy,
         thisOrganization: foundOrganization,
         allowedToEdit: !!foundOrganizationAdmin,
+        events: foundEvents,
       };
     }, [orgID]);
+    console.log(events);
     return (ready ? (
       <Container className="py-3 px-5" id={PAGE_IDS.VIEW_ORGANIZATION}>
         <Row className="justify-content-center">
@@ -39,7 +44,7 @@ const VolunteerOrganizations = () => {
                   <Card.Body>
                     <div className="d-flex justify-content-between">
                       <Card.Title>{thisOrganization.name}</Card.Title>
-                      {allowedToEdit && <EditOrgGear orgID={thisOrganization.orgID} />}
+                      {allowedToEdit && thisOrganization && <EditOrgGear orgID={thisOrganization.orgID} />}
                     </div>
                     <Card.Subtitle>Mission</Card.Subtitle>
                     <Card.Text>
@@ -55,27 +60,36 @@ const VolunteerOrganizations = () => {
                     <Card.Text>TestTag1 TestTag2</Card.Text>
                   </Card.Body>
                 </Card>
-                <Container className="py-3">
-                  <div className="d-flex justify-content-between">
-                    <h3>Upcoming events</h3>
-                    {allowedToEdit && (
-                      <Button
-                        variant="primary"
-                        className="rounded-circle d-flex justify-content-center align-items-center"
-                        style={{ width: '40px', height: '40px', marginLeft: '170px', marginBottom: '10px' }} // Adjust the pixel value as needed
-                        onClick={() => navigate(`/organizations/${thisOrganization.orgID}/add-event`)}
-                        id={COMPONENT_IDS.NAVBAR_ADD_EVENT}
-                      >
-                        <i className="fas fa-plus" />
-                      </Button>
-                    )}
-                  </div>
-                  Horizontal scroll of events
-                </Container>
-                <Container>
-                  <h3>Active opportunities</h3>
-                  List of opportunities
-                </Container>
+                <div className="d-flex justify-content-between mt-3">
+                  <h3>Upcoming events</h3>
+                  {allowedToEdit && (
+                    <Button
+                      variant="primary"
+                      className="rounded-circle d-flex justify-content-center align-items-center"
+                      style={{ width: '40px', height: '40px', marginLeft: '170px', marginBottom: '10px' }} // Adjust the pixel value as needed
+                      onClick={() => navigate(`/organizations/${thisOrganization.orgID}/add-event`)}
+                      id={COMPONENT_IDS.NAVBAR_ADD_EVENT}
+                    >
+                      <i className="fas fa-plus" />
+                    </Button>
+                  )}
+                </div>
+                <div className="d-flex flex-row overflow-x-auto">
+                  {events.map(event => (
+                    <div key={event._id} className="me-3">
+                      <Card style={{ width: '20rem', minHeight: '25rem' }}>
+                        <Card.Header>{event.name}</Card.Header>
+                        <Card.Body>
+                          event info
+                        </Card.Body>
+                      </Card>
+                    </div>
+                  ))}
+                </div>
+                {/* <Container> */}
+                {/* <h3>Active opportunities</h3> */}
+                {/* List of opportunities */}
+                {/* </Container> */}
               </>
             ) : <NotFound />}
           </Col>
