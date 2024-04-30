a6ce37a2797f20031ad08256c0ae2e574b6c57c9
Kristine Orpilla <143130393+kristineorpilla03@users.noreply.github.com>
2024-04-30 09:42:04 -1000

    Changed default data and fixed the events

--- app/imports/ui/pages/events/VolunteerEvents.jsx
+++ app/imports/ui/pages/events/VolunteerEvents.jsx
@@ -14,7 +14,7 @@ const VolunteerEvents = () => {
     const subscription2 = EventCategories.subscribeEventCategories();
     const subscription3 = Categories.subscribeCategory();
     const rdy = subscription.ready() && subscription2.ready() && subscription3.ready();
-    const eventItems = Events.find({}, { sort: { name: 1 } }).fetch();
+    const eventItems = Events.find({}, { sort: { eventDate: 1 } }).fetch();
     const eventCategoriesItems = EventCategories.find({}, { sort: { eventInfo: 1 } }).fetch();
     const categoriesItems = Categories.find({}, { sort: { categoryName: 1 } }).fetch();
     return {
